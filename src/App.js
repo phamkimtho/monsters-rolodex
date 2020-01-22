49075a8dfe464934929a1970627d91a2f6c1b8ac
Pham Kim Tho <tho.phamkim@gmail.com>
2020-01-22 14:59:38 +0700

    added input html element

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -20,9 +21,18 @@ class App extends Component {
   }
 
   render () {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className='App'>
-        <CardList monsters={this.state.monsters} />
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,6 @@ import './card-list.style.css'
 import {Card} from './card/card.component';
 
 export const CardList = props => {
-  console.log(props)
   return (
     <div className='card-list'>
       {props.monsters.map(monster => (
