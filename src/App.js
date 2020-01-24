0d3a6573eea9f61b39b44131f24510c5f39ba157
Pham Kim Tho <tho.phamkim@gmail.com>
2020-01-24 07:40:25 +0700

    added font and background

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
     )
     return (
       <div className='App'>
+        <h1>Monsters Rododex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={e => this.setState({ searchField: e.target.value })}
