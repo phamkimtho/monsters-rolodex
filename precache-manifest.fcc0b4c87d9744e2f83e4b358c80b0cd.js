7d51a5903fb473f41426ac65ef9fda14759c03a3
Pham Kim Tho <tho.phamkim@gmail.com>
2020-01-24 10:09:23 +0700

    Updates

---
+++ precache-manifest.fcc0b4c87d9744e2f83e4b358c80b0cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c4d2b7f1ef6476adbb8c87c4c25d8d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bed9b0ddb620537ad368",
+    "url": "/monsters-rolodex/static/css/main.419ff5b5.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bed9b0ddb620537ad368",
+    "url": "/monsters-rolodex/static/js/main.55b691b1.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
